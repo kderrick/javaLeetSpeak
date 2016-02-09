5143275bb71e91649988e03f4d2b0b98c82b0994
Blair Peterson and Kyle Derrick <kderrick1988@gmail.com>
2016-02-08 16:18:48 -0800

    WIP: UI?

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,10 +2,21 @@ import java.util.*;
 import static spark.Spark.*;
 import java.util.HashMap;
 import spark.ModelAndView;
-// import spark.template.velocity.VelocityTemplateEngine;
+import spark.template.velocity.VelocityTemplateEngine;
 
 public class App {
   public static void main(String[] args) {
+    String layout = "templates/layout.vtl";
+
+    get("/", (request, response) -> {
+        HashMap model = new HashMap();
+        String userInput = request.queryParams("userInput");
+
+        String results = App.leetTranslator(userInput);
+        model.put("results", results);
+        model.put("template", "templates/index.vtl");
+        return new ModelAndView(model, layout);
+     }, new VelocityTemplateEngine());
 
   }
 
@@ -16,5 +27,5 @@ public class App {
     userString = userString.replace("o", "0");
     return userString.replace("s","z");
   }
-  
+
 }
